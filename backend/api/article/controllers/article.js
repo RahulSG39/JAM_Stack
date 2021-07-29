84f69f80e21f3e3f59f278ae9c7ae3438039e139
RahulSG39 <rahulgirish39@gmail.com>
2021-07-29 20:03:33 +0530

    Policies

---
+++ backend/api/article/controllers/article.js
@@ -0,0 +1,7 @@
+"use strict";
+
+module.exports = {
+  async hello(ctx) {
+    return "Hello Rahul";
+  },
+};
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'ac6cd5a3-55aa-44b4-893c-4b6dde6b2790'
+};
